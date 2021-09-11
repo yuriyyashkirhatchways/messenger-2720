19df29388399b7d5f82a01f18606e1ec484d0a8c
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-09-10 21:27:49 -0400

    WIP database redesign

--- server/messenger_backend/models/message.py
+++ server/messenger_backend/models/message.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from . import utils
 from .conversation import Conversation
+from .user import User
 
 
 class Message(utils.CustomModel):
@@ -16,3 +17,4 @@ class Message(utils.CustomModel):
     )
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
+    readBy = models.ManyToManyField(User)
